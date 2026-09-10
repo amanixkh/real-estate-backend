6fd984b9f72d335d9ecc47a2def69796f0c17b15
amanixkh <xamtrk00@gmail.com>
2026-09-10 04:50:44 +0300

    Initial setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Real Estate Backend API");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
