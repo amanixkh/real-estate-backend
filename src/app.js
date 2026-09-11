01f062da3e988ee9ee57b07db256a42422d44234
zahraaqasiim <zahraaqasiim.3@gmail.com>
2026-09-11 21:25:04 +0300

    Implement property management with CRUD, categories, image uploads and database setup

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const categoryRoutes = require("./routes/categoryRoutes");
+const propertyRoutes = require("./routes/propertyRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use("/api/categories", categoryRoutes);
+app.use("/api/properties", propertyRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Real Estate Backend API");
+    res.json({
+        message: "Real Estate API is running"
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.gitenv.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
---
+++ src/middleware/upLoadMiddleware.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+       const uniquName = Date.now() +
+        '-' + 
+        Math.round(Math.random() * 1E9) +
+         path.extname(file.originalname);}
+});
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const extension = allowedTypes.test
+    (path.extname(file.originalname).toLowerCase());
+    const mimeType = allowedTypes.test(file.mimetype);
+    if (extension && mimeType) {
+        cb(null, true);
+    } else {
+        cb(new Error('only image files are allowed'));
+    }
+};
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+});
+module.exports = upload;
